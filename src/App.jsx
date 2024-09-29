a7c85a52881445b0ba29fe011e3efed01fcdeb36
kunals12 <work.kunal12@gmail.com>
2024-09-29 17:44:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+
+function App() {
+
+  return (
+    <>
+      <TokenLaunchpad />
+    </>
+  )
+}
+
+export default App
