a14db6b6ab684a916db76d75bfc36a40b22af86c
kunals12 <work.kunal12@gmail.com>
2024-09-29 17:52:46 +0530

    wallet connection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import './App.css'
+import { useMemo } from "react";
 import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+
 
 function App() {
 
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
   return (
-    <>
-      <TokenLaunchpad />
-    </>
+    <div>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+          <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+          <TokenLaunchpad />
+          </WalletModalProvider>
+        </WalletProvider>
+
+      </ConnectionProvider>
+    </div>
   )
 }
 
